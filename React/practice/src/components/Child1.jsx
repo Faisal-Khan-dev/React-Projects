ea3eaa87caf44459dfb30ad70c0e4d9f8d74d484
Faisal-Khan-dev <faisalkhan24687531@gmail.com>
2026-01-30 20:48:23 +0500

    react practice

---
+++ React/practice/src/components/Child1.jsx
@@ -0,0 +1,32 @@
+// import React, { useState } from "react";
+
+// const Child1 = () => {
+//     let a = 10;
+
+//     // const child = () => {
+//     //     a = 20
+//     //     console.log("a" , a);
+        
+//     // }
+
+    
+//     const [state , setState] = useState("faisal");
+//     console.log(state);
+
+//     const userHandler = () => {
+//         setState("faisal khan")
+        
+        
+//     }
+    
+    
+
+//     return (
+//         <>
+//         <h1>hello {state}</h1>
+//         <button onClick={userHandler}>update</button>
+//         </>
+//     )
+// }
+
+// export default Child1
